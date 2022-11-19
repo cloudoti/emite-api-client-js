a1a0cec0b4f0308a7dfd0ec78eefd8b97fafc4c7
Daniel Roncal <daniel.roncal.87@gmail.com>
2022-11-18 20:43:35 -0500

    Set version 0.0.18

--- libreria/utils/documento.ts
+++ libreria/utils/documento.ts
@@ -1468,7 +1468,6 @@ export class Cabecera {
       fechaEmision: this._fechaEmision,
       horaEmision: this._horaEmision,
       tipoMoneda: this._tipoMoneda,
-      simboloMoneda: this._simboloMoneda,
       tipoCambio: this._tipoCambio,
       serie: this._serie,
       correlativo: this._correlativo,
